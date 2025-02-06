eff41b0c99518f31ceb2b6e348795e443e3bacbb
magn3054 <mdch20@student.aau.dk>
2025-02-06 12:42:23 +0100

    portfolio side startet

--- js/notifications.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('notify-btn').addEventListener('click', () => {
-    Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-            new Notification('You are subscribed to notifications!');
-        }
-    });
-});
