dfad77d5c067b8714448841d27dbafb063f8c2a1
magn3054 <mdch20@student.aau.dk>
2025-02-11 12:21:18 +0100

    ny test

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function () {
     
     if (!arrowContainer) return; // Safety check
     
-    if (window.scrollY > 50) { // Adjust threshold as needed
+    if (window.scrollY > 50) {
         arrowContainer.style.opacity = "0";
     } else {
         arrowContainer.style.opacity = "1";
